ca5ebdf85caf93090cee164456c19fd32f6b10ea
Asif Rahaman <asifrahaman162@gmail.com>
2024-03-25 19:40:00 +0530

    WIP: Added grouping of the routers

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,13 +1,29 @@
 package routes
 
 import (
-	"github.com/asifrahaman13/hirego/internal/handlers"
-	"github.com/gin-gonic/gin"
+    "github.com/asifrahaman13/hirego/internal/handlers"
+    "github.com/gin-gonic/gin"
 )
 
+func SetupV1Routes(router *gin.Engine) {
+    v1 := router.Group("/auth")
+    {
+        v1.GET("/users", handlers.UserHandler.GetUsers)
+        v1.POST("/signup", handlers.UserHandler.Signup)
+        v1.POST("/login", handlers.UserHandler.Login)
+        v1.GET("/userdata", handlers.UserHandler.ProtectedRoute)
+    }
+}
+
+func SetupV2Routes(router *gin.Engine) {
+    v2 := router.Group("/v2")
+    {
+        v2.GET("/users", handlers.UserHandler.GetUsers)
+       // More routes to be added here
+    }
+}
+
 func InitializeRoutes(router *gin.Engine) {
-   router.GET("/users", handlers.UserHandler.GetUsers)
-   router.POST("/signup", handlers.UserHandler.Signup)
-   router.POST("/login", handlers.UserHandler.Login)
-   router.GET("/userdata", handlers.UserHandler.ProtectedRoute)
+    SetupV1Routes(router)
+    SetupV2Routes(router)
 }
